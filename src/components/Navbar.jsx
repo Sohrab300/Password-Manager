fb7366a88acdb9d860483b79ccc466b32e270922
Sohrab300 <133908818+Sohrab300@users.noreply.github.com>
2025-01-28 13:40:02 +0530

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-green-200">
+      <div className="logo font-bold">Password Manager</div>
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
